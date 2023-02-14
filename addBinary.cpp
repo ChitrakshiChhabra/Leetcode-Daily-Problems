5ef278a0ab2a172698475b8a45435507df0d4dd0
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-02-14 19:38:29 +0530

    Day 26 of 100 days of coding

---
+++ addBinary.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string addBinary(string a, string b)
+{
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    string ans;
+    int carry = 0;
+    while (i >= 0 || j >= 0 || carry)
+    {
+        if (i >= 0)
+        {
+            carry += a[i] - '0';
+            i--;
+        }
+        if (j >= 0)
+        {
+            carry += b[j] - '0';
+            j--;
+        }
+        ans += (carry % 2 + '0');
+        carry = carry / 2;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    cout << addBinary("11", "1");
+
+    return 0;
+}
