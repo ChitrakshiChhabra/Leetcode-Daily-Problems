8c32611d4a37d174a9b2eed5e2c1748fddb761ea
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-02-16 18:56:43 +0530

    Day 28 of 100 days of coding

---
+++ maxDepthOfBinaryTree.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int maxLeft = maxDepth(root->left);
+    int maxRight = maxDepth(root->right);
+    return max(maxLeft, maxRight) + 1;
+}
+
+int main()
+{
+
+    
+    return 0;
+}
