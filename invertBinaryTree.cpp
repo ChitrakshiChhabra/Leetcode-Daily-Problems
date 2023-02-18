b194809a479ef5ee9612b53ef46b1f6dc90dea0c
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-02-18 14:20:39 +0530

    Day 30 of 100 days of coding

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ TreeNode* invertTree(TreeNode* root) {
+        // Base Case
+        if(root==NULL)
+            return NULL;
+        invertTree(root->left); //Call the left substree
+        invertTree(root->right); //Call the right substree
+        // Swap the nodes
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        return root; // Return the root
+        
+    }
+
+    int main()
+    {
+        return 0;
+    }
