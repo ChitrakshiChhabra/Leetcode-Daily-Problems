26726f6affc61a92723d94b2431a275f76c78c4f
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-03-09 20:08:27 +0530

    Day 49 of 100 days of coding

---
+++ linkedListCycleII.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *detectCycle(ListNode *head)
+{
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return nullptr;
+}
+
+int main()
+{
+    return 0;
+}
