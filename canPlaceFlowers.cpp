759de6b2c0993d0a2e31925ec9cfc58d5cbab650
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-03-20 20:48:23 +0530

    Day 60 of 100 days of coding

---
+++ canPlaceFlowers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool canPlaceFlowers(vector<int> &flowerbed, int n)
+{
+    int count = 0;
+    for (int i = 0; i < flowerbed.size(); i++)
+    {
+        if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+        {
+            flowerbed[i] = 1;
+            count++;
+        }
+        if (count >= n)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector <int> flowerbed = {1,0,0,0,1};
+    cout << canPlaceFlowers(flowerbed, 2);
+    return 0;
+}
