e7565c9a17754167e9c372cbe5c7d457eaf3db8c
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-03-30 20:51:53 +0530

    Day 70 of 100 days of coding

---
+++ longestConsecutiveSequence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int longestConsecutive(vector<int> &nums)
+{
+    set<int> hashSet;
+    for (int num : nums)
+    {
+        hashSet.insert(num);
+    }
+    int longestStreak = 0;
+    for (int num : nums)
+    {
+        if (!hashSet.count(num - 1))
+        {
+            int currentNum = num;
+            int currentStreak = 1;
+
+            while (hashSet.count(currentNum + 1))
+            {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+}
+int main()
+{
+    return 0;
+}
