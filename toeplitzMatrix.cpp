963f689086cf77a7c83e270d3e53c5148b18cb61
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-04-08 19:33:04 +0530

    Day 79 of 100 days of coding

---
+++ toeplitzMatrix.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+bool isToeplitzMatrix(vector<vector<int>> &matrix)
+{
+    stack<pair<int, int>> st;
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        st.push({i, 0});
+    }
+    for (int i = 0; i < matrix[0].size(); i++)
+    {
+        st.push({0, i});
+    }
+
+    while (st.size())
+    {
+        int x = st.top().first;
+        int y = st.top().second;
+        int val = matrix[x][y];
+        st.pop();
+        while (x < matrix.size() && y < matrix[0].size())
+        {
+            if (matrix[x][y] != val)
+                return false;
+            x++, y++;
+        }
+    }
+
+    return true;
+}
+int main()
+{
+    return 0;
+}
