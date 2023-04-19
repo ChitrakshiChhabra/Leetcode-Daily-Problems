268b558e212152afc9c822c61d5ce03ec11f13e6
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-04-19 21:43:46 +0530

    Day 90 of 100 days of coding

---
+++ checkIfArrayIsSortedOrRotated.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool check(vector<int> &nums)
+{
+    int count = 0;
+    int n = nums.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            count++;
+        }
+    }
+    if (nums[n - 1] > nums[0])
+    {
+        count++;
+    }
+    return count <= 1;
+}
+int main()
+{
+    vector <int> nums = {3,4,5,1,2};
+    cout << check(nums);
+    return 0;
+}
