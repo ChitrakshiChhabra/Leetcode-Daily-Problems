481864aa011b6cd4f6d920cca48166bff2c9db14
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-04-24 22:54:28 +0530

    Day 95 of 100 days of coding

---
+++ lastStoneWeight.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int lastStoneWeight(vector<int> &stones)
+{
+    int n = stones.size();
+    priority_queue<int> max_heap;
+    for (auto stone : stones)
+    {
+        max_heap.push(stone);
+    }
+    while (max_heap.size() != 1)
+    {
+        int stone1 = max_heap.top();
+        max_heap.pop();
+        int stone2 = max_heap.top();
+        max_heap.pop();
+        max_heap.push(max(stone1, stone2) - min(stone1, stone2));
+    }
+    return max_heap.top();
+}
+int main()
+{
+    return 0;
+}
